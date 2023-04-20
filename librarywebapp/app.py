ab1f36bed283ac7c50dfeb58ff045549a0ead1ac
SherlynDuan <118089268+SherlynDuan@users.noreply.github.com>
2023-04-20 21:19:35 +1200

    css

--- librarywebapp/app.py
+++ librarywebapp/app.py
@@ -14,6 +14,7 @@ app = Flask(__name__)
 
 dbconn = None
 connection = None
+user= None
 
 def getCursor():
     global dbconn
@@ -23,114 +24,132 @@ def getCursor():
     database=connect.dbname, autocommit=True)
     dbconn = connection.cursor()
     return dbconn
+    
 
 #  The public interface
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("login.html")
 
-#Search the available books by title and/or author, See the availability of all copies of a book, whether a copy is on loan and, if so, the due date.
-@app.route ("/search")
-def search():
-    return render_template ("search.html")
+@app.route("/homepage", methods=["GET"])
+def homepage():
+    member=request.args.get("member") 
+    staff=request.args.get("staff")
+    global user
+    if member:
+        user=member
+        return render_template("memberHome.html", member=user)
+    elif staff:
+        user=staff
+        return render_template("staffHome.html", staff=user)
+    return render_template("login.html")
 
-# show results of the book search, get input value from forms, allow for partial text searches
-@app.route ("/searchresult", methods=["POST"])
-def searchresult():
-    author=request.form.get("author")
-    authorsearch = "%" + author + "%"
-    title =request.form.get("title")
-    titlesearch = "%" + title + "%"
-    connection = getCursor()
-
-# an if condition to test which input is empty
-# if the author input is empty   
-    if author == '':
-        connection.execute("SELECT books.bookid, books.booktitle, books.author, bookcopies.bookcopyid, bookcopies.format,\
-       loans.returned, DATEDIFF(CURDATE(),loans.loandate) \
-        FROM bookcopies LEFT JOIN books on books.bookid = bookcopies.bookid LEFT JOIN loans \
-        on bookcopies.bookcopyid=loans.bookcopyid WHERE booktitle LIKE %s;",(titlesearch,)) 
-        result_list = connection.fetchall()
-# if the title input is empty      
-    elif title == '':
-        connection.execute("SELECT books.bookid, books.booktitle, books.author, bookcopies.bookcopyid, bookcopies.format,\
-       loans.returned, DATEDIFF(CURDATE(),loans.loandate) \
-        FROM bookcopies LEFT JOIN books on books.bookid = bookcopies.bookid LEFT JOIN loans \
-        on bookcopies.bookcopyid=loans.bookcopyid WHERE author LIKE %s;",(authorsearch,)) 
-        result_list= connection.fetchall()
-
-    else:
-        connection.execute("SELECT books.bookid, books.booktitle, books.author, bookcopies.bookcopyid, bookcopies.format,\
-       loans.returned, DATEDIFF(CURDATE(),loans.loandate) \
-        FROM bookcopies LEFT JOIN books on books.bookid = bookcopies.bookid LEFT JOIN loans \
-        on bookcopies.bookcopyid=loans.bookcopyid WHERE author LIKE %s And booktitle LIKE %s;",(authorsearch,titlesearch,)) 
-        result_list= connection.fetchall()
-    
-    print (result_list)
-
-    return render_template ("result.html", author=author, title=title, result_list = result_list )
-    
-# List all books available in the library at the /booklist route Include book title, author,category and year of publication.                
-@app.route("/listbooks")
-def listbooks():
+@app.route ("/allbooks")
+def allbooks():
     connection = getCursor()
     connection.execute("SELECT bookid, booktitle, author, category, \
     yearofpublication FROM books ;")
     bookList = connection.fetchall()
-    print(bookList)
-    return render_template("booklist.html", booklist = bookList)  
-
-# homepage of the staff interface
-@app.route("/staff")
-def staff():
-    return render_template("staffhome.html") 
+    return render_template ("allbooks.html",  booklist = bookList, member=user)
 
 #Search the available books by title and/or author, See the availability of all copies of a book, whether a copy is on loan and, if so, the due date.
+
 @app.route ("/search")
-@app.route("/staff/search")
-def staffsearch():
-    return render_template("staffsearch.html")
+def search():
+    return render_template("search.html", member=user)
 
-## show results of the book search, get input value from forms, allow for partial text searches. 
-@app.route ("/staffsearchresult", methods=["POST"])
-def staffsearchresult():
-    author=request.form.get("author")
-    authorsearch = "%" + author + "%"
-    title =request.form.get("title")
-    titlesearch = "%" + title + "%"
+# show results of the book search, get input value from forms, allow for partial text searches
+@app.route ("/searchresult", methods=["POST"])
+def searchresult():
+    searchinput=request.form.get("searchinput")
+    searchinput = "%" + searchinput + "%"
     connection = getCursor()
-    
-    if author == '':
-        connection.execute("SELECT books.bookid, books.booktitle, books.author, bookcopies.bookcopyid, bookcopies.format,\
-       loans.returned, DATEDIFF(CURDATE(),loans.loandate) \
-        FROM bookcopies LEFT JOIN books on books.bookid = bookcopies.bookid LEFT JOIN loans \
-        on bookcopies.bookcopyid=loans.bookcopyid WHERE booktitle LIKE %s;",(titlesearch,)) 
-        result_list = connection.fetchall()
-# if the title input is empty      
-    elif title == '':
-        connection.execute("SELECT books.bookid, books.booktitle, books.author, bookcopies.bookcopyid, bookcopies.format,\
-       loans.returned, DATEDIFF(CURDATE(),loans.loandate) \
-        FROM bookcopies LEFT JOIN books on books.bookid = bookcopies.bookid LEFT JOIN loans \
-        on bookcopies.bookcopyid=loans.bookcopyid WHERE author LIKE %s;",(authorsearch,)) 
-        result_list= connection.fetchall()
 
-    else:
-        connection.execute("SELECT books.bookid, books.booktitle, books.author, bookcopies.bookcopyid, bookcopies.format,\
-       loans.returned, DATEDIFF(CURDATE(),loans.loandate) \
-        FROM bookcopies LEFT JOIN books on books.bookid = bookcopies.bookid LEFT JOIN loans \
-        on bookcopies.bookcopyid=loans.bookcopyid WHERE author LIKE %s And booktitle LIKE %s;",(authorsearch,titlesearch,)) 
-        result_list= connection.fetchall()
-    
+    connection.execute("SELECT books.bookid, books.booktitle, books.author, bookcopies.bookcopyid, bookcopies.format,\
+    loans.returned, DATEDIFF(CURDATE(),loans.loandate) \
+    FROM bookcopies LEFT JOIN books on books.bookid = bookcopies.bookid LEFT JOIN loans \
+    on bookcopies.bookcopyid=loans.bookcopyid WHERE booktitle LIKE %s OR author LIKE %s;",(searchinput, searchinput, )) 
+    result_list = connection.fetchall()
     print (result_list)
 
-    return render_template ("staffsearch_result.html", author=author, title=title, result_list = result_list )
+    return render_template ("result.html", result_list = result_list,  member=user)
+@app.route("/staff/allbooks")
+def staffallbooks():
+    return render_template("staffallbooks.html", staff=user)
 
- 
+#Search the available books by title and/or author, See the availability of all copies of a book, whether a copy is on loan and, if so, the due date.
+@app.route("/staff/searchbook")
+def staffsearchbook():
+    return render_template("staffsearchbook.html", staff=user)
+
+## show results of the book search, get input value from forms, allow for partial text searches. 
+@app.route ("/staffsearchbookresult", methods=["POST"])
+def staffsearchresult():
+    searchinput=request.form.get("searchinput")
+    searchinput = "%" + searchinput + "%"
+    connection = getCursor()
+
+    connection.execute("SELECT books.bookid, books.booktitle, books.author, bookcopies.bookcopyid, bookcopies.format,\
+    loans.returned, DATEDIFF(CURDATE(),loans.loandate) \
+    FROM bookcopies LEFT JOIN books on books.bookid = bookcopies.bookid LEFT JOIN loans \
+    on bookcopies.bookcopyid=loans.bookcopyid WHERE booktitle LIKE %s OR author LIKE %s;",(searchinput, searchinput, )) 
+    result_list = connection.fetchall()
+
+
+    return render_template ("result.html", result_list = result_list, staff=user)
+
+ #Issue a book to a borrower. Physical Books can only be loaned once at a time , eBooks and Audio Books can be loaned multiple times simultaneously
+#staff type in book copy id and borrower id
+@app.route("/staff/issuebooks")
+def issuebooks():
+    return render_template("issuebooks.html", staff=user)
+
+#show results of issuing books and update database.
+@app.route( "/staff/issuebooks_result",  methods=["GET", "POST"])
+def issuebooks_result():
+    bookcopyid=request.form.get("bookid")
+    borrowerid=request.form.get("borrowerid")
+    todaydate = datetime.now().date()
+    connection = getCursor()
+    connection.execute ( "SELECT bookcopies.bookcopyid, bookcopies.format, loans.borrowerid, loans.returned, loans.loandate From loans RIGHT JOIN bookcopies \
+        on loans.bookcopyid=bookcopies.bookcopyid WHERE bookcopies.bookcopyid = %s ORDER by loans.loandate DESC LIMIT 1  ;", (bookcopyid, ) )
+    loans= connection.fetchall()
+    print (loans)
+    #an if condition to test if the book is a Physical one or not
+    if loans[0][1] == "eBook" or loans[0][1] == "Audio Book" :
+        connection = getCursor()
+        connection.execute ( " INSERT INTO loans (bookcopyid, borrowerid , loandate, returned) VALUES (%s, %s,%s,0);", (bookcopyid, borrowerid, todaydate, ) )
+        return render_template ("issuebook_success.html", bookcopyid=bookcopyid, borrowerid=borrowerid, todaydate=todaydate, staff=user)
+
+    elif (loans[0][1] == "Hardcover" or "paperback" or "Illustrated") and (loans[0][3] == 1 or loans[0][3] == None):
+        connection = getCursor()
+        connection.execute ( " INSERT INTO loans (bookcopyid, borrowerid , loandate, returned) VALUES (%s, %s,%s,0);", (bookcopyid, borrowerid, todaydate, ) )
+        return render_template("issuebook_success.html", bookcopyid=bookcopyid, borrowerid=borrowerid, todaydate=todaydate, staff=user)
+        
+    else:
+        return render_template( "issuebook_fail.html", staff=user)
+
+
+#Return a book that has been on loan
+@app.route("/staff/returnbooks")
+def returnbooks():
+    return render_template("returnbooks.html", staff=user) 
+
+#return results
+@app.route("/staff/returnbooks_result",  methods=["GET", "POST"] )
+def returnbooks_result():
+    bookcopyid=request.form.get("bookid")
+    borrowerid=request.form.get("borrowerid")
+    todaydate = datetime.now().date()
+    connection = getCursor()
+    connection.execute ( " UPDATE loans SET returned = 1 WHERE bookcopyid= %s AND borrowerid= %s;", (bookcopyid, borrowerid, ))
+    return render_template("return_success.html", borrowerid=borrowerid, bookcopyid=bookcopyid, todaydate=todaydate, staff=user )
+
 
 # View the details of a borrower, searching by name or by borrower id.
 @app.route("/staff/borrowersearch")
 def borrowersearch():
-    return render_template("borrowersearch.html") 
+    return render_template("borrowersearch.html", staff=user) 
 
 #show borrowe rsearch results
 
@@ -156,13 +175,13 @@ def borrowersearchresult():
         result_list = connection.fetchall()
     
     
-    return render_template ("borrowerresult.html", name=name, borrowerid=borrowerid, result_list = result_list)
+    return render_template ("borrowerresult.html", staff=user,name=name, borrowerid=borrowerid, result_list = result_list)
                 
 
 # Update the details of a borrower
 @app.route("/staff/updateborrower")
 def updateborrower():
-    return render_template("updateborrower.html") 
+    return render_template("updateborrower.html", staff=user) 
 
 
 # get the borrower id that the staff wants to update
@@ -171,10 +190,10 @@ def staffborrowerid():
     id=request.form.get("id")
     connection = getCursor()
     connection.execute ("SELECT * from borrowers WHERE borrowerid = %s;", (id,) )
-    results= connection.fetchall()
+    results= connection.fetchone()
     print (results)
 
-    return render_template("iddetails.html", id=id, results=results) 
+    return render_template("iddetails.html", id=id, results=results, staff=user ) 
 
 #upda information to the database
 @app.route("/staff/updateinfo",  methods=["GET", "POST"])
@@ -196,14 +215,14 @@ def staffupdateinfo():
     connection.execute ( "SELECT * FROM borrowers WHERE borrowerid= %s;" , (ID, ) )
     results_list = connection.fetchall()
     print (results_list)
-    return render_template ("updateconfirm.html", ID=ID, results_list= results_list)
+    return render_template ("updateconfirm.html", ID=ID, results_list= results_list, staff=user)
 
 
  
 #Add a new borrower
 @app.route("/staff/addborrower")
 def addborrower():
-    return render_template("addborrower.html")
+    return render_template("addborrower.html", staff=user)
 
 
 #get all input value from users and insert into database
@@ -225,56 +244,12 @@ def addborrowerinfo():
     results_list = connection.fetchall()
 
     print (results_list)
-    return render_template ("addconfirm.html", results_list= results_list)
+    return render_template ("addconfirm.html", results_list= results_list, staff=user)
 
-#Issue a book to a borrower. Physical Books can only be loaned once at a time , eBooks and Audio Books can be loaned multiple times simultaneously
-#staff type in book copy id and borrower id
-@app.route("/staff/issuebooks")
-def issuebooks():
-    return render_template("issuebooks.html")
+@app.route("/staff/reports")
+def reports():
+    return render_template ("reports.html", staff=user)
 
-#show results of issuing books and update database.
-@app.route( "/staff/issuebooks_result",  methods=["GET", "POST"])
-def issuebooks_result():
-    bookcopyid=request.form.get("bookid")
-    borrowerid=request.form.get("borrowerid")
-    todaydate = datetime.now().date()
-    connection = getCursor()
-    connection.execute ( "SELECT bookcopies.bookcopyid, bookcopies.format, loans.borrowerid, loans.returned, loans.loandate From loans RIGHT JOIN bookcopies \
-        on loans.bookcopyid=bookcopies.bookcopyid WHERE bookcopies.bookcopyid = %s ORDER by loans.loandate DESC LIMIT 1  ;", (bookcopyid, ) )
-    loans= connection.fetchall()
-    print (loans)
-    #an if condition to test if the book is a Physical one or not
-    if loans[0][1] == "eBook" or loans[0][1] == "Audio Book" :
-        connection = getCursor()
-        connection.execute ( " INSERT INTO loans (bookcopyid, borrowerid , loandate, returned) VALUES (%s, %s,%s,0);", (bookcopyid, borrowerid, todaydate, ) )
-        return render_template ("issuebook_success.html", bookcopyid=bookcopyid, borrowerid=borrowerid, todaydate=todaydate)
-
-    elif (loans[0][1] == "Hardcover" or "paperback" or "Illustrated") and (loans[0][3] == 1 or loans[0][3] == None):
-        connection = getCursor()
-        connection.execute ( " INSERT INTO loans (bookcopyid, borrowerid , loandate, returned) VALUES (%s, %s,%s,0);", (bookcopyid, borrowerid, todaydate, ) )
-        return render_template("issuebook_success.html", bookcopyid=bookcopyid, borrowerid=borrowerid, todaydate=todaydate)
-        
-    else:
-        return render_template( "issuebook_fail.html")
-
-
-#Return a book that has been on loan
-@app.route("/staff/returnbooks")
-def returnbooks():
-    return render_template("returnbooks.html") 
-
-#return results
-@app.route("/staff/returnbooks_result",  methods=["GET", "POST"] )
-def returnbooks_result():
-    bookcopyid=request.form.get("bookid")
-    borrowerid=request.form.get("borrowerid")
-    todaydate = datetime.now().date()
-    connection = getCursor()
-    connection.execute ( " UPDATE loans SET returned = 1 WHERE bookcopyid= %s AND borrowerid= %s;", (bookcopyid, borrowerid, ))
-    return render_template("return_success.html", borrowerid=borrowerid, bookcopyid=bookcopyid, todaydate=todaydate )
-
-    
 #Display a list of all overdue books & their borrowers. Group all overdue books by borrower 
 @app.route("/staff/overduebooks")
 def overduebooks():
@@ -286,7 +261,7 @@ def overduebooks():
         WHERE loans.returned = 0 and DATEDIFF(CURDATE(),loans.loandate) >=35;")
     overdue_list=connection.fetchall()
     print ( overdue_list)    
-    return render_template("overduebooks.html", overdue_list=overdue_list) 
+    return render_template("overduebooks.html", overdue_list=overdue_list, staff=user) 
 
 #Display a list (Loan Summary) showing the number of times each book has been loaned in total.
 @app.route("/staff/loansummary")
@@ -297,7 +272,7 @@ def loansummary():
         GROUP BY bookcopies.bookid ORDER BY COUNT(loans.loanid) DESC;")
     loansummary=connection.fetchall()
 
-    return render_template("loansummary.html", loansummary=loansummary) 
+    return render_template("loansummary.html", loansummary=loansummary, staff=user) 
 
 #Display a list (Borrower Summary) showing all borrowers and the number of loans (past and current combined) in total they each have had.
 @app.route("/staff/borrowersummary")
@@ -306,8 +281,7 @@ def borrowersummary():
     connection.execute (" SELECT borrowers.borrowerid, borrowers.firstname,borrowers.familyname, COUNT(loans.loanid) FROM loans LEFT JOIN borrowers \
         ON borrowers.borrowerid=loans.borrowerid GROUP BY loans.borrowerid ORDER BY COUNT(loans.loanid) DESC;")
     borrowersummary=connection.fetchall()
-    return render_template("borrowersummary.html", borrowersummary=borrowersummary)
+    return render_template("borrowersummary.html", borrowersummary=borrowersummary, staff=user) 
 
-# Code to enable debug mode 
 if __name__ == '__main__':
     app.run(debug=True)
